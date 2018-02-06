1382b0d5badbf5111103ceb767d2468780232840
Feng Lu <linkcd@gmail.com>
2018-02-06 20:29:46 +0100

    Update Verify infra-as-code.ps1

--- Real-life-infra-as-code/Verify infra-as-code.ps1
+++ Real-life-infra-as-code/Verify infra-as-code.ps1
@@ -2,5 +2,5 @@
     Write-Host $Env:BUILD_SOURCESDIRECTORY 
 	Write-Host $Env:BUILD_SOURCESDIRECTORY\Real-life-infra-as-code\azuredeploy.json
 
-
+write-HOST 11
 Test-AzureRmResourceGroupDeployment -ResourceGroupName 'Real-life-infra-as-code-manual-testing' -TemplateFile $Env:BUILD_SOURCESDIRECTORY\Real-life-infra-as-code\azuredeploy.json -TemplateParameterFile $Env:BUILD_SOURCESDIRECTORY\Real-life-infra-as-code\azuredeploy.nightlybuild.parameters.json

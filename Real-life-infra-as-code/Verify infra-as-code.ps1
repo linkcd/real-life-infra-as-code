9603f441b59e2211650790466401f5d17dd7fe8b
Feng Lu <linkcd@gmail.com>
2018-02-06 14:10:49 +0100

    added verify powershell

---
+++ Real-life-infra-as-code/Verify infra-as-code.ps1
@@ -0,0 +1,1 @@
+Test-AzureRmResourceGroupDeployment -ResourceGroupName 'Real-life-infra-as-code-manual-testing' -TemplateFile 'azuredeploy.json' -TemplateParameterFile 'azuredeploy.nightlybuild.parameters.json'
